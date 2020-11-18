4155d0fe9cd1ef0f09c50050519675bbde4f8c64
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-11-18 19:51:30 +0530

    Added: get List method in Open CSV Builder

--- src/main/java/CSVBuilderJar/CSVBuilderJar/OpenCSVBuilder.java
+++ src/main/java/CSVBuilderJar/CSVBuilderJar/OpenCSVBuilder.java
@@ -1,6 +1,7 @@
 package CSVBuilderJar.CSVBuilderJar;
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
@@ -9,11 +10,27 @@ public class OpenCSVBuilder<E> implements ICSVBuilder {
 	
 	public Iterator<E> getCsvFileIterator(Reader reader, Class csvClass) throws CSVBuilderException {
 		try {
+			OpenCSVBuilder openCSVBuilder = new OpenCSVBuilder();
+			Iterator<E> myIterator = openCSVBuilder.getCSVBean(reader, csvClass).iterator();
+			return myIterator;
+		}
+		catch (RuntimeException e) {
+			e.printStackTrace();
+			throw new CSVBuilderException(CSVBuilderException.ExceptionType.UNABLE_TO_PARSE, "Unable to Parse");
+		}
+	}
+
+	public List getCSVFileList(Reader reader, Class csvClass) throws CSVBuilderException {
+		return this.getCSVBean(reader, csvClass).parse();
+	}
+	
+	private CsvToBean<E> getCSVBean(Reader reader, Class csvClass) throws CSVBuilderException {
+		try {
 			CsvToBean<E> csvToBean = new CsvToBeanBuilder<E>(reader)
 	                    .withType(csvClass)
 	                    .withIgnoreLeadingWhiteSpace(true)
 	                    .build();
-			return csvToBean.iterator();
+			return csvToBean;
 		}
 		catch (RuntimeException e) {
 			e.printStackTrace();
